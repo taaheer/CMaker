6a237d6eb1aa9395d499adbde07b655660d9d1c8
taaheer <taaheer@gmail.com>
2024-07-05 22:45:20 +0000

    implement empty file generating

--- CMaker.cpp
+++ CMaker.cpp
@@ -2,23 +2,37 @@
 #include "CLI/Formatter.hpp"
 #include "CLI/Config.hpp"
 
+#include <fstream>
 #include <iostream>
+
+constexpr std::string gFilename{"CMakeLists.txt"};
 
 void generateCMake([[maybe_unused]]std::size_t count)
 {
-    std::cout << "Generating CMake\n";
+    std::cout << "Generating " << gFilename << '\n';
+
+    std::ofstream cmake{gFilename};
+    if (!cmake.is_open())
+    {
+        std::cerr << "Failed to generate " << gFilename << '\n';
+        return;
+    }
+    
+    cmake.close();
+
+    std::cout << gFilename << " generated successfully\n";
 }
 
 void removeCMake([[maybe_unused]]std::size_t count)
 {
-    std::cout << "Removing CMake\n";
+    std::cout << "Removing " << gFilename << '\n';
 }
 
 int main(int argc, char **argv) 
 {
     if(argc == 1)
     {
-        std::cout << "Error: no operation specified (use -h or --help for help)\n";
+        std::cerr << "Error: no operation specified (use -h or --help for help)\n";
         return -1;
     }
 
@@ -26,9 +40,9 @@ int main(int argc, char **argv)
 
     app.set_version_flag("-v, --version", "0.1");
 
-    app.add_flag("-M, --make", generateCMake, "Generate CMakeLists.txt");
+    app.add_flag("-M, --make", generateCMake, "Generate " + gFilename);
 
-    app.add_flag("-R, --remove", removeCMake, "Remove existing CMakeLists.txt");
+    app.add_flag("-R, --remove", removeCMake, "Remove existing " + gFilename);
 
     CLI11_PARSE(app, argc, argv);
 
