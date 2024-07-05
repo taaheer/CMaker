c5c8f04af6184fc32f81edec6762be97b47ace9c
taaheer <taaheer@gmail.com>
2024-07-05 22:24:30 +0000

    if no tags given then give error message and exit

--- CMaker.cpp
+++ CMaker.cpp
@@ -16,13 +16,19 @@ void removeCMake([[maybe_unused]]std::size_t count)
 
 int main(int argc, char **argv) 
 {
+    if(argc == 1)
+    {
+        std::cout << "Error: no operation specified (use -h or --help for help)\n";
+        return -1;
+    }
+
     CLI::App app{"CMaker is CLI CMake generator"};
 
     app.set_version_flag("-v, --version", "0.1");
 
-    app.add_flag("-M, --make", generateCMake, "Generate CMake.txt");
+    app.add_flag("-M, --make", generateCMake, "Generate CMakeLists.txt");
 
-    app.add_flag("-R, --remove", removeCMake, "Remove existing CMake.txt");
+    app.add_flag("-R, --remove", removeCMake, "Remove existing CMakeLists.txt");
 
     CLI11_PARSE(app, argc, argv);
 
