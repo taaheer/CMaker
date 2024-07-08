bdf0da22745078c6537f64283f165cf815d77178
taaheer <taaheer@gmail.com>
2024-07-08 21:12:35 +0000

    made seperate class for cmake

---
+++ CMake.h
@@ -0,0 +1,26 @@
+#ifndef CMAKE_H
+#define CMAKE_H
+
+#include <string>
+
+constexpr std::string gFilename{"CMakeLists.txt"};
+
+class CMake
+{
+    private:
+    std::string defaultVersion{"3.7...3.29"};
+    const double leastMinimumVersion{3.0};
+    const double latestAvailableVersion{3.3};
+
+    std::string addHeader() const;
+    std::string getCMakeVersion() const;
+    bool isCMakeVersionValid(const std::string& input) const;
+    bool isNumeric(const std::string& input) const;
+
+    public:
+    
+    void generateCMake([[maybe_unused]]std::size_t count) const;
+    void removeCMake([[maybe_unused]]std::size_t count) const;
+};
+
+#endif
